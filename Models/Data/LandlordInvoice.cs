247a749de9cac0665c8e98c5da97cce5f303ade2
Mogomotsi <olifantmogomotsi@gmail.com>
2024-03-26 12:57:15 +0200

    changes

---
+++ Models/Data/LandlordInvoice.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SafriSoftv1._3.Models.Data
+{
+    public class LandlordInvoice
+    {
+        public int Id { get; set; }
+        // Landlord Invoice Details
+        //public string 
+    }
+}
